f4cd1b3ecff277e1a94e9634f216363f885d133f
Willian Rodrigues <willianrdrigues@hotmail.com>
2023-08-12 15:22:03 -0300

    Adicionando Menu

--- flappy.py
+++ flappy.py
@@ -16,6 +16,35 @@ PIPE_HEIGHT = 500
 
 PIPE_GAP = 200
 
+class Button():
+	def __init__(self, x, y, image, scale):
+		width = image.get_width()
+		height = image.get_height()
+		self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
+		self.rect = self.image.get_rect()
+		self.rect.topleft = (x, y)
+		self.clicked = False
+
+	def draw(self, surface):
+		action = False
+		#get mouse position
+		pos = pygame.mouse.get_pos()
+
+		#check mouseover and clicked conditions
+		if self.rect.collidepoint(pos):
+			if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+				self.clicked = True
+				action = True
+
+		if pygame.mouse.get_pressed()[0] == 0:
+			self.clicked = False
+
+		#draw button on screen
+		surface.blit(self.image, (self.rect.x, self.rect.y))
+
+		return action
+
+
 class Bird(pygame.sprite.Sprite):
 
     def __init__(self):
@@ -105,6 +134,9 @@ def restart_button():
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+start_img = pygame.image.load('start_btn.png').convert_alpha()
+exit_img = pygame.image.load('exit_btn.png').convert_alpha()
+
 BACKGROUND = pygame.image.load('background-day.png')
 BACKGROUND = pygame.transform.scale(BACKGROUND, (SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -126,62 +158,93 @@ for i in range(2):
 
 clock = pygame.time.Clock()
 
+start_button = Button(100, 200, start_img, 0.8)
+exit_button = Button(115, 380, exit_img, 0.8)
 
-while True:
-    clock.tick(30)
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
 
-        if event.type == KEYDOWN:
-            if event.key == K_SPACE:
-                bird.bump()
+def initGame():
+    bird_group.empty()
+    bird = Bird()
+    bird_group.add(bird)
 
-    screen.blit(BACKGROUND, (0, 0))
+    ground_group.empty()
+    for i in range(2):
+        ground = Ground(GROUND_WIDTH * i)
+        ground_group.add(ground)
 
-    if is_off_screen(ground_group.sprites()[0]):
-        ground_group.remove(ground_group.sprites()[0])
-
-        new_ground = Ground(GROUND_WIDTH - 20)
-        ground_group.add(new_ground)
-
-    if is_off_screen(pipe_group.sprites()[0]):
-        pipe_group.remove(pipe_group.sprites()[0])
-        pipe_group.remove(pipe_group.sprites()[0])
-
-        pipes = get_random_pipes(SCREEN_WIDTH * 2)
-
+    pipe_group.empty()
+    for i in range(2):
+        pipes = get_random_pipes(SCREEN_WIDTH * i + 800)
         pipe_group.add(pipes[0])
         pipe_group.add(pipes[1])
 
-    bird_group.update()
-    ground_group.update()
-    pipe_group.update()
-
-    bird_group.draw(screen)
-    pipe_group.draw(screen)
-    ground_group.draw(screen)
-
-    pygame.display.update()
-
-    if (pygame.sprite.groupcollide(bird_group, ground_group, False, False, pygame.sprite.collide_mask) or
-       pygame.sprite.groupcollide(bird_group, pipe_group, False, False, pygame.sprite.collide_mask)):
-        # Game over
-        time.sleep(2)
+    while True:
+        clock.tick(30)
         
-        # Reinicia o jogo
-        bird_group.empty()
-        bird = Bird()
-        bird_group.add(bird)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
 
-        ground_group.empty()
-        for i in range(2):
-            ground = Ground(GROUND_WIDTH * i)
-            ground_group.add(ground)
+            if event.type == KEYDOWN:
+                if event.key == K_SPACE:
+                    bird.bump()
 
-        pipe_group.empty()
-        for i in range(2):
-            pipes = get_random_pipes(SCREEN_WIDTH * i + 800)
+        screen.blit(BACKGROUND, (0, 0))
+
+        if is_off_screen(ground_group.sprites()[0]):
+            ground_group.remove(ground_group.sprites()[0])
+
+            new_ground = Ground(GROUND_WIDTH - 20)
+            ground_group.add(new_ground)
+
+        if is_off_screen(pipe_group.sprites()[0]):
+            pipe_group.remove(pipe_group.sprites()[0])
+            pipe_group.remove(pipe_group.sprites()[0])
+
+            pipes = get_random_pipes(SCREEN_WIDTH * 2)
+
             pipe_group.add(pipes[0])
             pipe_group.add(pipes[1])
-    
+
+        bird_group.update()
+        ground_group.update()
+        pipe_group.update()
+
+        bird_group.draw(screen)
+        pipe_group.draw(screen)
+        ground_group.draw(screen)
+
+        pygame.display.update()
+
+        if (pygame.sprite.groupcollide(bird_group, ground_group, False, False, pygame.sprite.collide_mask) or
+                pygame.sprite.groupcollide(bird_group, pipe_group, False, False, pygame.sprite.collide_mask)):
+            # Game over
+            bird_group.empty()
+            ground_group.empty()
+            pipe_group.empty()
+            time.sleep(1)
+            return
+        
+def show_menu():
+    run = True
+    while run:
+
+        screen.fill((202, 228, 241))
+
+        if start_button.draw(screen):
+            initGame()
+        
+        if exit_button.draw(screen):
+            pygame.quit()
+
+        #event handler
+        for event in pygame.event.get():
+            #quit game
+            if event.type == pygame.QUIT:
+                run = False
+
+        pygame.display.update()
+
+    pygame.quit()
+
+show_menu()
