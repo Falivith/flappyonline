c95a27f923511082231017405d7d9af0424bc9b7
Willian Rodrigues <willianrdrigues@hotmail.com>
2023-08-18 01:28:29 -0300

    Add hover no menu

--- flappy.py
+++ flappy.py
@@ -20,32 +20,32 @@ PIPE_GAP = 200
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 class Button():
-	def __init__(self, x, y, image, scale):
-		width = image.get_width()
-		height = image.get_height()
-		self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
-		self.rect = self.image.get_rect()
-		self.rect.topleft = (x, y)
-		self.clicked = False
+    def __init__(self, x, y, image, scale):
+        width = image.get_width()
+        height = image.get_height()
+        self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+        self.clicked = False
 
-	def draw(self, surface):
-		action = False
-		#get mouse position
-		pos = pygame.mouse.get_pos()
+    def draw(self, surface):
+        action = False
+        pos = pygame.mouse.get_pos()
 
-		#check mouseover and clicked conditions
-		if self.rect.collidepoint(pos):
-			if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
-				self.clicked = True
-				action = True
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and not self.clicked:
+                self.clicked = True
+                action = True
+            elif pygame.mouse.get_pressed()[0] == 0:
+                self.clicked = False
+            surface.blit(self.image, (self.rect.x, self.rect.y))
+            pygame.draw.rect(surface, (0, 200, 0, 100), self.rect, pygame.BLEND_RGBA_MULT)
+        else:
+            self.clicked = False
+            surface.blit(self.image, (self.rect.x, self.rect.y))
 
-		if pygame.mouse.get_pressed()[0] == 0:
-			self.clicked = False
+        return action
 
-		#draw button on screen
-		surface.blit(self.image, (self.rect.x, self.rect.y))
-
-		return action
 
 class Bird(pygame.sprite.Sprite):
 
